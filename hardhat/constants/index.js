080b4086c3c10ebdffed71de38594466a2311446
Raj Lathigra <rajlathigra03@gmail.com>
2023-05-01 12:25:39 +0530

    Uploaded

---
+++ hardhat/constants/index.js
@@ -0,0 +1,6 @@
+// Address of the Whitelist Contract that you deployed
+const WHITELIST_CONTRACT_ADDRESS = "0xBfFdd9E227d828E6C60a0681b4Cc7283C6F13Eac";
+// URL to extract Metadata for a Crypto Dev NFT
+const METADATA_URL = "https://nft-collection-sneh1999.vercel.app/api/";
+
+module.exports = { WHITELIST_CONTRACT_ADDRESS, METADATA_URL };
